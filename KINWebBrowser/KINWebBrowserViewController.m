62c4cf3c213343c18e457302f4b8e66c67250a1a
scaryterry <pissed.sphinx@gmail.com>
2015-03-04 00:12:00 +0200

    Update KINWebBrowserViewController.m
    Added method  and property that will be used when displaying an NSString as a website

--- KINWebBrowser/KINWebBrowserViewController.m
+++ KINWebBrowser/KINWebBrowserViewController.m
@@ -43,7 +43,7 @@ static void *KINContext = &KINContext;
 @property (nonatomic, strong) UIBarButtonItem *backButton, *forwardButton, *refreshButton, *stopButton, *actionButton, *fixedSeparator, *flexibleSeparator;
 @property (nonatomic, strong) NSTimer *fakeProgressTimer;
 @property (nonatomic, strong) UIPopoverController *actionPopoverController;
-@property (nonatomic, assign) BOOL uiWebViewIsLoading;
+@property (nonatomic, assign) BOOL uiWebViewIsLoading, hideToolbar;
 @property (nonatomic, strong) NSURL *uiWebViewCurrentURL;
 
 @end
@@ -151,11 +151,12 @@ static void *KINContext = &KINContext;
     [super viewWillAppear:animated];
     
     [self.navigationController setNavigationBarHidden:NO animated:YES];
-    [self.navigationController setToolbarHidden:NO animated:YES];
-    
+    if (self.hideToolbar == false) // will be true if displaying an NSString as website with [self loadText:text] in which case the toolbar is unnecessary
+    {
+        [self.navigationController setToolbarHidden:NO animated:YES];
+        [self updateToolbarState];
+    }
     [self.navigationController.navigationBar addSubview:self.progressView];
-    
-    [self updateToolbarState];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
@@ -170,6 +171,21 @@ static void *KINContext = &KINContext;
 }
 
 #pragma mark - Public Interface
+
+- (void)loadText:(NSString *)text {
+    NSString* htmlContentString = [NSString stringWithFormat:
+                                   @"<html>"
+                                   "<body>"
+                                   "<p>%@</p>"
+                                   "</body></html>", text];
+    if(self.wkWebView) {
+        [self.wkWebView loadHTMLString:htmlContentString baseURL:nil ];
+    }
+    else if(self.uiWebView) {
+        [self.uiWebView loadHTMLString:htmlContentString baseURL:nil ];
+    }
+    self.hideToolbar = true; 
+}
 
 - (void)loadURL:(NSURL *)URL {
     if(self.wkWebView) {
