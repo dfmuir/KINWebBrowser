eba71bab4516eb9b143360ddc54894a3f761b560
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-09-09 21:28:37 +0200

    supported interface orientations returns UIInterfaceOrientationMask instead of NSUInteger to eliminate Xcode warnings

--- KINWebBrowser/KINWebBrowserViewController.m
+++ KINWebBrowser/KINWebBrowserViewController.m
@@ -596,7 +596,7 @@ static void *KINWebBrowserContext = &KINWebBrowserContext;
 
 #pragma mark - Interface Orientation
 
-- (NSUInteger)supportedInterfaceOrientations {
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
     return UIInterfaceOrientationMaskAllButUpsideDown;
 }
 
